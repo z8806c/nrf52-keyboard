201e63e5c14a58e1720cb48a7a164f2369a03d2a
zhangcheng <5267621@qq.com>
2021-11-25 21:36:02 +0800

    1

--- keyboard/Hypertoxic/common/keymap_common.h
+++ keyboard/Hypertoxic/common/keymap_common.h
@@ -35,24 +35,24 @@ extern const action_t fn_actions[];
 #define KEYMAP( \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-              K42, K43,      K45, K46, K47,      K49, K4A       \
+    K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
+    K40, K41, K42,           K45,                K49, K4A, K4B  \
 ) { \
     { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B }, \
     { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B }, \
-    { KC_##K30, KC_NO   , KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B }, \
-    { KC_NO   , KC_NO   , KC_##K42, KC_##K43, KC_NO   , KC_##K45, KC_##K46, KC_##K47, KC_NO   , KC_##K49, KC_##K4A, KC_NO    }  \
+    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B }, \
+    { KC_##K40, KC_##K41, KC_##K42, KC_NO,    KC_NO,    KC_##K45, KC_NO,    KC_NO,    KC_NO,    KC_##K49, KC_##K4A, KC_##K4B }  \
 }
 
 /* ANSI variant. No extra keys for ISO */
 #define KEYMAP_ANSI( \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-              K42, K43,      K45, K46, K47,      K49, K4A       \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,      \
+    K30, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C,  \
+    K40, K41, K42,           K45,                K49, K4A, K4B  \
 ) KEYMAP( \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30, NO , K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
-    NO , NO , K42, K43, NO , K45, K46, K47, NO , K49, K4A, NO  \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B,\
+    K30, NO,  K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, \
+    K40, K41, K42,           K45,                K49,  K4A, K4B  \
 )
